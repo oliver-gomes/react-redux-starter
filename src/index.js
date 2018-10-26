c514bab8229647a8ee72ed678dea6e3ae8e980db
Oliver Gomes <ogomesphoto@gmail.com>
2018-10-25 21:35:46 -0400

    API update

--- src/index.js
+++ src/index.js
@@ -3,17 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import SearchBar from "./components/search_bar";
 import YTSearch from "youtube-api-search";
+import VideoList from "./components/video_list";
 const API_KEY = "AIzaSyCXj2uFzyDkNtiicp0NHoK22I9BchUcN8M";
 
-YTSearch({ key: API_KEY, term: "surfboards" }, function(data) {
-  console.log(data);
-});
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
